943025712ed9c21a5da6d1720721bbb54a484164
J <jacksonhick330@gmail.com>
2023-03-14 14:20:17 +0000

    End of section 8

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -2,24 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using API.Extensions;
 
-namespace API.Entities
+namespace API.DTOs
 {
-    public class AppUser
+    public class MemberDto
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        public byte[] PasswordHash {get; set;}
-        public byte[] PasswordSalt {get; set;}
 
-        public DateOnly DateOfBirth { get; set; }
+        public string PhotoUrl { get; set; }
+
+        public int Age { get; set; }
 
         public string KnownAs { get; set; }
 
-        public DateTime Created { get; set; } = DateTime.UtcNow; 
+        public DateTime Created { get; set; }
 
-        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; }
 
         public string Gender { get; set; }
 
@@ -33,14 +32,6 @@ namespace API.Entities
 
         public string Country { get; set; }
 
-        public List<Photo> Photos { get; set; } = new();
-
-
-        /* public int GetAge()
-        {
-            return DateOfBirth.CalculateAge();
-        }
- */
-        
+        public List<PhotoDto> Photos { get; set; }
     }
 }
