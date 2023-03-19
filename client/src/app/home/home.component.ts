4675d91eff2200d654111feae5668932777ac643
J <jacksonhick330@gmail.com>
2023-03-19 14:39:49 +0000

    lesson 111 backup commit

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -11,24 +11,15 @@ export class HomeComponent implements OnInit {
   registerMode = false;
   users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.GetUsers();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
 
-  GetUsers()
-   {
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    })
-   }
    cancelRegisterMode(event: boolean) {
     this.registerMode = event;
    }
