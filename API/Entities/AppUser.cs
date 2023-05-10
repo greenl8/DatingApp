a46fece2ee40fedd190382512d9272e364767a92
J <jacksonhick330@gmail.com>
2023-05-10 12:46:09 +0100

    Section 16 modal issue

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,16 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Extensions;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Entities
 {
-    public class AppUser
+    public class AppUser : IdentityUser<int>
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public byte[] PasswordHash {get; set;}
-        public byte[] PasswordSalt {get; set;}
-
         public DateOnly DateOfBirth { get; set; }
 
         public string KnownAs { get; set; }
@@ -42,6 +38,8 @@ namespace API.Entities
         public List<Message> MessagesSent { get; set; }
 
         public List<Message> MessagesReceived { get; set; }
+
+        public ICollection<AppUserRole> UserRoles { get; set; }
 
         /* public int GetAge()
         {
