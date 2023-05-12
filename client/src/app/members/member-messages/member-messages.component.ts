2971dfdc6296077c04e772ff7aaee0045f27df3a
J <jacksonhick330@gmail.com>
2023-05-12 12:42:12 +0100

    End of Section 17

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -11,11 +11,10 @@ import { MessageService } from 'src/app/_services/message.service';
 export class MemberMessagesComponent implements OnInit {
   @ViewChild('messageForm') messageForm?: NgForm
   @Input() username?: string;
-  @Input() messages: Message[] = [];
   messageContent = '';
 
 
-  constructor(private messageService: MessageService) { }
+  constructor(public messageService: MessageService) { }
 
 
 
@@ -25,13 +24,10 @@ export class MemberMessagesComponent implements OnInit {
 
   sendMessage() {
     if(!this.username) return;
-    this.messageService.sendMessage(this.username, this.messageContent).subscribe({
-      next: message => {
-        this.messages.push(message);
-        this.messageForm?.reset();
-      
-      }
-    })
+    this.messageService.sendMessage(this.username, this.messageContent).then(() => {
+      this.messageForm?.reset();
+    });
   }
+  
 
 }
