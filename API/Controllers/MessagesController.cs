fa4cd9b571cc2f44f93511605dec1eface8585a2
J <jacksonhick330@gmail.com>
2023-05-14 10:48:53 +0100

    End of section 18

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -16,15 +16,15 @@ namespace API.Controllers
     [Authorize]
     public class MessagesController : BaseApiController
     {
-        private readonly IMapper _mapper;
-        private readonly IUserRepository _userRepository;
-        private readonly IMessageRepository _messageRepository;
 
-        public MessagesController(IUserRepository userRepository, IMessageRepository messageRepository, IMapper mapper)
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+
+        public MessagesController(IMapper mapper, IUnitOfWork uow)
         {
-            _messageRepository = messageRepository;
-            _userRepository = userRepository;
             _mapper = mapper;
+            _uow = uow;
+
         }
 
         [HttpPost]
@@ -35,8 +35,8 @@ namespace API.Controllers
             if (username == createMessageDto.RecipientUsername.ToLower())
                 return BadRequest("You cannot send messages to yourself");
 
-            var sender = await _userRepository.GetUserByUsernameAsync(username);
-            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+            var sender = await _uow.UserRepository.GetUserByUsernameAsync(username);
+            var recipient = await _uow.UserRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
 
             if (recipient == null) return NotFound();
 
@@ -49,9 +49,9 @@ namespace API.Controllers
                 content = createMessageDto.Content
             };
 
-            _messageRepository.AddMessage(message);
+            _uow.MessageRepository.AddMessage(message);
 
-            if (await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
+            if (await _uow.Complete()) return Ok(_mapper.Map<MessageDto>(message));
 
             return BadRequest("Failed to send message");
 
@@ -62,7 +62,7 @@ namespace API.Controllers
         MessageParams messageParams)
         {
             messageParams.Username = User.GetUsername();
-            var messages = await _messageRepository.GetMessagesForUser(messageParams);
+            var messages = await _uow.MessageRepository.GetMessagesForUser(messageParams);
 
             Response.AddPaginationHeader(new PaginationHeader(messages.CurrentPage, messages.PageSize, messages.TotalCount, messages.TotalPages));
 
@@ -70,19 +70,12 @@ namespace API.Controllers
         }
 
 
-        [HttpGet("thread/{username}")]
-        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesThread(string username)
-        {
-            var currentUsername = User.GetUsername();
-
-            return Ok(await _messageRepository.GetMessageThread(currentUsername, username));
-        }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteMessage(int id)
         {
             var username = User.GetUsername();
-            var message = await _messageRepository.GetMessage(id);
+            var message = await _uow.MessageRepository.GetMessage(id);
 
             if(message.SenderUsername != username && message.RecipientUsername != username) 
                 return Unauthorized();
@@ -92,10 +85,10 @@ namespace API.Controllers
 
             if(message.SenderDeleted && message.RecipientDeleted)
             {
-                _messageRepository.DeleteMessage(message);
+                _uow.MessageRepository.DeleteMessage(message);
             }
 
-            if(await _messageRepository.SaveAllAsync()) return Ok();
+            if(await _uow.Complete()) return Ok();
 
             return BadRequest("Problem deleting message");
         }
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -27,10 +27,9 @@ namespace API.Extensions
               services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
               services.AddScoped<IPhotoService, PhotoService>();
               services.AddScoped<LogUserActivity>();
-              services.AddScoped<ILikesRepository, LikesRepository>();
-              services.AddScoped<IMessageRepository, MessageRepository>();
               services.AddSignalR();
               services.AddSingleton<PresenceTracker>();
+              services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 
               return services;
